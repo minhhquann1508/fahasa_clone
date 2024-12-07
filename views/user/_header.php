a48bbd9d1955c2feb8548abe06a2caf7c2f53e1c
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2024-12-07 13:25:48 +0700

    update header categoríe

--- views/user/_header.php
+++ views/user/_header.php
@@ -1,21 +1,73 @@
- <div>
+<?php
+$data['category'] = [
+    1 => [
+        'name' => 'Electronics',
+        'thumbnail' => 'https://example.com/images/electronics.jpg',
+        'slug' => 'electronics',
+    ],
+    2 => [
+        'name' => 'Books',
+        'thumbnail' => 'https://example.com/images/books.jpg',
+        'slug' => 'books',
+    ],
+    3 => [
+        'name' => 'Clothing',
+        'thumbnail' => 'https://example.com/images/clothing.jpg',
+        'slug' => 'clothing',
+    ],
+    4 => [
+        'name' => 'Furniture',
+        'thumbnail' => 'https://example.com/images/furniture.jpg',
+        'slug' => 'furniture',
+    ],
+    5 => [
+        'name' => 'Toys',
+        'thumbnail' => 'https://example.com/images/toys.jpg',
+        'slug' => 'toys',
+    ],
+];
+
+?>
+
+
+<div>
     <div style="background-color:var(--secondary)" class="d-flex justify-content-center">
         <div class="container">
             <img src="./img/top_banner.webp" height="auto" width="100%">
         </div>
     </div>
     <div class="mx-auto" style="background-color: white">
-        <div class="container" style="width:1230px">
+        <div class="container p-0" style="width:1230px">
             <div class="d-flex align-items-center justify-content-between" style="height: 68px;">
                 <div>
                     <img src="./img/logo.webp" style="width:220px; vertical-align: middle;">
                 </div>
-                <div class="d-flex gap-2">
-                    <div class="d-flex align-items-center gap-1 position-relative dropdown-wrapper">
-                        <img src="./img/menu_red.svg" style="color: var(--greyD);">
-                        <span><i class="fa-solid fa-angle-down"></i></span>
+                <div class="d-flex gap-3">
+                    <div class="d-flex align-items-center gap-2 position-relative"  style="height:68px">
+                        <div class="dropdown d-flex align-items-center gap-2"  style="height:100%">
+                            <img src="./img/menu_red.svg" style="color: var(--greyD);">
+                            <span><i class="fa-solid fa-angle-down"></i></span>
+                        <ul class="dropdown-menu container pt-4 px-3 pb-3" aria-labelledby="dropdownMenuButton"  style="width:1230px">
+                            <div class="row">
+                                <div class="col-3 " style="border-right:1px solid #ccc">
+                                    <p class="fs-4">Danh mục sản phẩm</p>
+                                    <?php foreach ($data['category'] as $keys => $values) : ?>
+                                        <li>
+                                            <a class="dropdown-item" href="#">
+                                                <?php echo $values['name']; ?>
+                                            </a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </div>
+                                <div class="col-9">
+
+                                </div>
+                            </div>
+
+                        </ul>
+                        </div>
                     </div>
-                    <div class="input-group">
+                    <div class="input-group d-flex align-items-center">
                         <div class="input-group">
                             <input type="text" class="form-control rounded" placeholder="Tìm kiếm sản phẩm..." style="width: 585px;height:40px">
                             <div class="input-group-append position-relative">
@@ -54,26 +106,18 @@
         </div>
     </div>
 </div>
-<div id="dropdown">
-    sadaklsjdklsval
-</div>
 
 <style>
-#dropdown {
-    display: none; 
-    position: absolute;
-    top: 100%; 
-    left: 0;
-    background: white;
-    border: 1px solid var(--greyD);
-    padding: 10px;
-    z-index: 1000;
-    width: 100%; 
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+.dropdown:hover>.dropdown-menu {
+  display: block;
 }
 
-.dropdown-wrapper:hover #dropdown {
-    display: block;
+.dropdown>.dropdown-toggle:active {
+  /*Without this, clicking will make it sticky*/
+    pointer-events: none;
 }
-
+.dropdown-menu {
+    left: -255px;
+    top: 65px;
+}
 </style>
